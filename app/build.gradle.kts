1a17b33c8c3b9530f6d54d0615f146764e8beffa
Sourav B M <Sourav.BM@microchip.com>
2026-05-17 08:52:04 +0000

    fix: bump compileSdk/targetSdk to 35 (required by core-ktx 1.15.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.sourav.balancewidget"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sourav.balancewidget"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
